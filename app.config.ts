d091cd74c8f9a0d4ad90b6c97cd74887b8097706
Norin Chamroeun <53612429+NYT92@users.noreply.github.com>
2023-08-03 18:45:32 +0700

    new blog

--- app.config.ts
+++ app.config.ts
@@ -1,38 +1,47 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
-    description: 'The minimalist blog theme',
+    title: 'NYT92',
+    description: 'NYT92 official blog',
     image: {
-      src: '/social-card-preview.png',
-      alt: 'An image showcasing my project.',
+      src: '/nyt92-bh.svg',
+      alt: 'bg',
       width: 400,
       height: 300
     },
     header: {
       position: 'right', // possible value are : | 'left' | 'center' | 'right'
       logo: {
-        path: '/logo.svg', // path of the logo
-        pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'alpine' // alt of the logo
+        path: '/nyt92-logo-light.svg', // path of the logo
+        pathDark: '/nyt92-logo.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
+        alt: 'nyt92-logo' // alt of the logo
       }
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
-        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+        // possible value are : true | false
+        enabled: false,
+
+        // our github repository
+        repository: 'https://www.github.com/nuxt-themes/alpine',
+
+        text: ''
       },
       navigation: true, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+      message: 'Find me at' // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
-      twitter: 'nuxtlabs',
-      instagram: 'atinuxt',
+      twitter: 'nyt92',
+      instagram: '',
+
       linkedin: {
         icon: 'uil:linkedin',
         label: 'LinkedIn',
         href: 'https://www.linkedin.com/company/nuxtlabs'
-      }
+      },
+
+      github: 'nyt92',
+      youtube: 'nyt92official'
     },
     form: {
       successMessage: 'Message sent. Thank you!'
